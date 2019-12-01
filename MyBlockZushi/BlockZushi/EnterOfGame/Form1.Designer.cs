25383ed69e75be7883ad40fc3bb0bdd2d8585408
OnHTB <on-mc-33@ezweb.ne.jp>
2019-12-01 15:59:55 +0900

    tukareta

---
+++ MyBlockZushi/BlockZushi/EnterOfGame/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace EnterOfGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ball = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.BtnStart = new System.Windows.Forms.Label();
+            this.Sushi1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Transparent;
+            this.ball.ForeColor = System.Drawing.Color.Transparent;
+            this.ball.Image = ((System.Drawing.Image)(resources.GetObject("ball.Image")));
+            this.ball.Location = new System.Drawing.Point(667, 153);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(17, 18);
+            this.ball.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.BackColor = System.Drawing.Color.LightCoral;
+            this.BtnStart.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.BtnStart.Location = new System.Drawing.Point(372, 213);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(57, 25);
+            this.BtnStart.TabIndex = 3;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // Sushi1
+            // 
+            this.Sushi1.BackColor = System.Drawing.Color.Transparent;
+            this.Sushi1.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Sushi1.Image = ((System.Drawing.Image)(resources.GetObject("Sushi1.Image")));
+            this.Sushi1.Location = new System.Drawing.Point(298, 99);
+            this.Sushi1.Name = "Sushi1";
+            this.Sushi1.Size = new System.Drawing.Size(50, 18);
+            this.Sushi1.TabIndex = 4;
+            this.Sushi1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(298, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 5;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(298, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 18);
+            this.label2.TabIndex = 6;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.Location = new System.Drawing.Point(298, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 18);
+            this.label3.TabIndex = 7;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+            this.label4.Location = new System.Drawing.Point(298, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 18);
+            this.label4.TabIndex = 8;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
+            this.label5.Location = new System.Drawing.Point(246, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 18);
+            this.label5.TabIndex = 9;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
+            this.label6.Location = new System.Drawing.Point(246, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 18);
+            this.label6.TabIndex = 10;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Image = ((System.Drawing.Image)(resources.GetObject("label7.Image")));
+            this.label7.Location = new System.Drawing.Point(246, 135);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 18);
+            this.label7.TabIndex = 11;
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label8.Image = ((System.Drawing.Image)(resources.GetObject("label8.Image")));
+            this.label8.Location = new System.Drawing.Point(246, 153);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 18);
+            this.label8.TabIndex = 12;
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label9.Image = ((System.Drawing.Image)(resources.GetObject("label9.Image")));
+            this.label9.Location = new System.Drawing.Point(246, 169);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 18);
+            this.label9.TabIndex = 13;
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label10.Image = ((System.Drawing.Image)(resources.GetObject("label10.Image")));
+            this.label10.Location = new System.Drawing.Point(197, 99);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(50, 18);
+            this.label10.TabIndex = 14;
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label11.Image = ((System.Drawing.Image)(resources.GetObject("label11.Image")));
+            this.label11.Location = new System.Drawing.Point(197, 117);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(50, 18);
+            this.label11.TabIndex = 15;
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label12.Image = ((System.Drawing.Image)(resources.GetObject("label12.Image")));
+            this.label12.Location = new System.Drawing.Point(197, 135);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(50, 18);
+            this.label12.TabIndex = 16;
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label13.Image = ((System.Drawing.Image)(resources.GetObject("label13.Image")));
+            this.label13.Location = new System.Drawing.Point(197, 153);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(50, 18);
+            this.label13.TabIndex = 17;
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            this.label14.BackColor = System.Drawing.Color.Transparent;
+            this.label14.Font = new System.Drawing.Font("游ゴシック", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label14.Image = ((System.Drawing.Image)(resources.GetObject("label14.Image")));
+            this.label14.Location = new System.Drawing.Point(197, 169);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(50, 18);
+            this.label14.TabIndex = 18;
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.ClientSize = new System.Drawing.Size(818, 450);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Sushi1);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.ball);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label BtnStart;
+        private System.Windows.Forms.Label Sushi1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ MyBlockZushi/BlockZushi/EnterOfGame/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnterOfGame
+{
+    public partial class Form1 : Form
+    {
+        int x = 0; //フォームの横幅
+        int y = 0; //フォームの縦幅
+        int nowTop = 0; //上座標の差
+        int nowLeft = 0; //左座標の差
+        String direction = "左上"; //ボールの動く向き
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nowTop = ball.Top; //現在のボールの上辺の位置を取得
+            nowLeft = ball.Left; //現在のボールの左辺の位置を取得
+
+            x = this.Width; //フォームの横幅を取得
+            y = this.Height; //フォームの高さを取得
+
+
+
+
+
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e) //スタートボタンクリック時の動作
+        {
+            timer1.Enabled = true; //タイマー起動
+            BtnStart.Visible = false; //ボタンを見えないようにする
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //タイマーが起動したら
+        {
+            int zahyouX = 7; //ずらしたいx座標の数値
+            int zahyouY = 3; //ずらしたいy座標の数値
+
+            if (ball.Left >= zahyouX && ball.Top >= zahyouY && direction == "左上")
+            {
+                ball.Top -= zahyouY; //ボールの左辺の座標をzahyou分ずらす　上方向へ
+                ball.Left -= zahyouX; //ボールの左辺の座標をzahyou分ずらす　左方向へ
+            }
+            else if (ball.Left >= zahyouX && ball.Bottom <= y - zahyouY && direction == "左下")
+            {
+                ball.Top += zahyouY; //ボールの上辺の座標をzahyou分ずらす　下方向へ
+                ball.Left -= zahyouX; //ボールの左辺の座標をzahyou分ずらす　左方向へ
+            }
+            else if (ball.Right <= x -zahyouX && ball.Bottom <= y - zahyouY && direction == "右下")
+            {
+                ball.Top += zahyouY; //ボールの上辺の座標をzahyou分ずらす　下方向へ
+                ball.Left += zahyouX; //ボールの左辺の座標をzahyou分ずらす　右方向へ
+            }
+            else if (ball.Right <= x - zahyouX && ball.Top >= zahyouY && direction == "右上")
+            {
+                ball.Top -= zahyouY; //ボールの上辺の座標をzahyou分ずらす　上方向へ
+                ball.Left += zahyouX; //ボールの左辺の座標をzahyou分ずらす　右方向へ
+            }
+            else if (0 < ball.Top && ball.Top < zahyouY) //もしボールの上辺の座標がForm上辺に達しそうになったら
+            {
+                ball.Top = 0; //ボールの上辺をFormの上辺にくっつける
+                if (direction == "左上") //ボールが左上に向かっていたら
+                {
+                    ball.Left -= zahyouX; //ボールの左辺の座標をzahyou分ずらす　左へ
+                }
+                else if (direction == "右上") //ボールが右上に向かっていたら
+                {
+                    ball.Left += zahyouX; //ボールの左辺の座標をzahyou分ずらす　右へ
+                }
+            }
+            else if (ball.Top == 0) //ボールがFormの上辺についたら
+            {
+                if (direction == "左上") //ボールが左上に向かっていたら
+                {
+                    ball.Top += zahyouY; //ボールの左辺の座標をzahyou分ずらす　下方向へ
+                    ball.Left -= zahyouX; //ボールの左辺の座標をzahyou分ずらす　左方向へ
+                    if (ball.Left < 0) //ボールがFormの左辺を超える場合
+                    {
+                        ball.Left = 0; //ボールをFormの左辺にくっつける
+                    }
+                    direction = "左下";
+                }
+                else if (direction == "右上") //ボールが右上に向かっていたら
+                {
+                    ball.Top += zahyouY; //ボールの左辺の座標をzahyou分ずらす　下方向へ
+                    ball.Left += zahyouX; //ボールの左辺の座標をzahyou分ずらす　右方向へ
+                    if (ball.Right > x) //ボールがFormの右辺を超える場合
+                    {
+                        ball.Left = x - ball.Size.Width; //ボールの右辺をFormの右辺にくっつける
+                    }
+                    direction = "右下";
+                }
+            }
+            else if (0 < ball.Left && ball.Left < zahyouX) //もしボールの左辺の座標がForm左辺に達しそうになったら
+            {
+                ball.Left = 0; //ボールの左辺をFormの左辺にくっつける
+
+                if (direction == "左下") //ボールが左下に向かっていたら
+                {
+                    ball.Top += zahyouY; //ボールの上辺の座標をzahyou分ずらす　下へ
+                }
+                else if (direction == "左上") //ボールが左上に向かっていたら
+                {
+                    ball.Top -= zahyouY; //ボールの上辺の座標をzahyou分ずらす　上へ
+                }
+            }
+            else if (ball.Left == 0) //ボールがFormの左辺についたら
+            {
+                if (direction == "左下") //ボールが左下に向かっていたら
+                {
+                    ball.Top += zahyouY; //ボールの上辺の座標をzahyou分ずらす　下方向へ
+                    ball.Left += zahyouX; //ボールの左辺の座標をzahyou分ずらす　右方向へ
+                    if (ball.Bottom > y) //ボールがFormの底辺を超える場合
+                    {
+                        ball.Top = y - ball.Size.Height; //ボールの底辺をFormの底辺にくっつける
+                    }
+                    direction = "右下";
+                }
+                else if (direction == "左上") //ボールが左上に向かっていたら
+                {
+                    ball.Top -= zahyouY; //ボールの上辺の座標をzahyou分ずらす　上方向へ
+                    ball.Left += zahyouX; //ボールの左辺の座標をzahyou分ずらす　右方向へ
+                    if (ball.Top < 0) //ボールがFormの上辺を超える場合
+                    {
+                        ball.Top = 0; //ボールの上辺をFormの上辺にくっつける
+                    }
+                    direction = "右上";
+                }
+            }
+            else if (y > ball.Bottom && ball.Bottom > y - zahyouY) //もしボールの底辺の座標がForm底辺に達しそうになったら
+            {
+                ball.Top = y - ball.Size.Height ; //ボールの底辺をFormの底辺にくっつける
+
+                if (direction == "右下") //ボールが右下に向かっていたら
+                {
+                    ball.Left += zahyouX; //ボールの左辺の座標をzahyou分ずらす　右へ
+                }
+                else if (direction == "左下") //ボールが左下に向かっていたら
+                {
+                    ball.Left -= zahyouX; //ボールの左辺の座標をzahyou分ずらす　左へ
+                }
+            }
+            else if (ball.Bottom == y) //ボールがFormの底辺についたら
+            {
+                if (direction == "左下" && ball.Left - zahyouX >= 0) //ボールが左下に向かっていたら
+                {
+                    ball.Top -= zahyouY; //ボールの上辺の座標をzahyou分ずらす　上方向へ
+                    ball.Left -= zahyouX; //ボールの左辺の座標をzahyou分ずらす　左方向へ
+                    if (ball.Left < 0) //ボールがFormの左辺を超える場合
+                    {
+                        ball.Left = 0; //ボールをFormの左辺にくっつける
+                    }
+                    direction = "左上";
+                }
+                else if (direction == "右下") //ボールが右下に向かっていたら
+                {
+                    ball.Top -= zahyouY; //ボールの上辺の座標をzahyou分ずらす　上方向へ
+                    ball.Left += zahyouX; //ボールの左辺の座標をzahyou分ずらす　右方向へ
+                    if (ball.Right > x) //ボールがFormの右辺を超える場合
+                    {
+                        ball.Left = x - ball.Size.Width; //ボールの右辺をFormの右辺にくっつける
+                    }                                                                          
+                    direction = "右上";
+                }
+            }
+            else if (x > ball.Right && ball.Right > x - zahyouX) //もしボールの右辺の座標がForm右辺に達しそうになったら
+            {
+                ball.Left = x - ball.Size.Width; //ボールの右辺をFormの右辺にくっつける
+
+                if (direction == "右上") //ボールが右上に向かっていたら
+                {
+                    ball.Top -= zahyouY; //ボールの上辺の座標をzahyou分ずらす　上へ
+                }
+                else if (direction == "右下") //ボールが右下に向かっていたら
+                {
+                    ball.Top += zahyouY; //ボールの上辺の座標をzahyou分ずらす　下へ
+                }
+            }
+            else if (ball.Right == x) //ボールがFormの右辺についたら
+            {
+                if (direction == "右上") //ボールが右上に向かっていたら
+                {
+                    ball.Top -= zahyouY; //ボールの上辺の座標をzahyou分ずらす　上方向へ
+                    ball.Left -= zahyouX; //ボールの左辺の座標をzahyou分ずらす　左方向へ
+                    if (ball.Top < 0) //ボールがFormの上辺を超える場合
+                    {
+                        ball.Top = 0; //ボールの上辺をFormの上辺にくっつける
+                    }
+                    direction = "左上";
+                }
+                else if (direction == "右下") //ボールが右下に向かっていたら
+                {
+                    ball.Top += zahyouY; //ボールの上辺の座標をzahyou分ずらす　下方向へ
+                    ball.Left -= zahyouX; //ボールの左辺の座標をzahyou分ずらす　左方向へ
+                    if (ball.Bottom > y) //ボールがFormの底辺を超える場合
+                    {
+                        ball.Top = y - ball.Size.Height; //ボールの底辺をFormの底辺にくっつける
+                    }
+                    direction = "左下";
+                }
+            }
+        }
+
+
+    }
+}
